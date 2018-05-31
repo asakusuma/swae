81f8197a15df8acf6186757716dc70f053b42a2c
asakusuma <asa.kusuma@gmail.com>
2018-05-30 18:30:58 -0700

    Turn back on debugging

--- test/acceptance/basic.spec.ts
+++ test/acceptance/basic.spec.ts
@@ -147,6 +147,9 @@ describe('Service Worker', () => {
     await session.run(async (testEnv) => {
       const client = testEnv.getActiveTabClient();
 
+      // Turn on to help debug test that is only failing in travis
+      client.debug();
+
       await client.navigate();
 
       await client.evaluate(function() {
